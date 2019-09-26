bdd8755362326e1c2329b9d87f77b053424bd4ef
Lyu Lyu <wl16298@bristol.ac.uk>
2019-09-26 18:16:19 +0100

    Add reaction flux calculation

--- depth_flatness/reaction_flux.py
+++ depth_flatness/reaction_flux.py
@@ -36,7 +36,7 @@ alpha=0.5
 #mu=alpha**(3/4)
 mu=0.1
 omega=1.0
-epsilon=0.05
+epsilon=0.1
 
 parameters = np.array([MASS_A, MASS_B, EPSILON_S, alpha, mu, epsilon, omega])
 eqNum = 1;  
@@ -84,26 +84,26 @@ f = partial(saddlenode_tpcd.saddlenode2dof, par=parameters)
 soln = solve_ivp(f, TSPAN, x0po_1,method='RK45',dense_output=True, events = saddlenode_tpcd.half_period,rtol=RelTol, atol=AbsTol)
 te = soln.t_events[0]
 tt = [0,te[2]]
-t,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_1,parameters)
+t1,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_1,parameters)
 x_po1 = x[:,0]
 y_po1 = x[:,1]
 px_po1 = x[:,2]
 py_po1 = x[:,3]
 
 f = partial(saddlenode_tpcd.saddlenode2dof, par=parameters) 
-soln = solve_ivp(f, TSPAN, x0po_1,method='RK45',dense_output=True, events = saddlenode_tpcd.half_period,rtol=RelTol, atol=AbsTol)
+soln = solve_ivp(f, TSPAN, x0po_2,method='RK45',dense_output=True, events = saddlenode_tpcd.half_period,rtol=RelTol, atol=AbsTol)
 te = soln.t_events[0]
 tt = [0,te[2]]
-t,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_1,parameters)
+t2,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_2,parameters)
 x_po2 = x[:,0]
 y_po2 = x[:,1]
 px_po2 = x[:,2]
 py_po2 = x[:,3]
 f = partial(saddlenode_tpcd.saddlenode2dof, par=parameters) 
-soln = solve_ivp(f, TSPAN, x0po_1,method='RK45',dense_output=True, events = saddlenode_tpcd.half_period,rtol=RelTol, atol=AbsTol)
+soln = solve_ivp(f, TSPAN, x0po_3,method='RK45',dense_output=True, events = saddlenode_tpcd.half_period,rtol=RelTol, atol=AbsTol)
 te = soln.t_events[0]
 tt = [0,te[2]]
-t,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_1,parameters)
+t3,x,phi_t1,PHI = saddlenode_tpcd.stateTransitMat_saddlenode(tt,x0po_3,parameters)
 x_po3 = x[:,0]
 y_po3 = x[:,1]
 px_po3 = x[:,2]
@@ -113,10 +113,12 @@ py_po3 = x[:,3]
 q1=0
 q2=0
 q3=0
-for i in range(len(x[:,3])-1):
-    q1 = q1+ 0.5*(t[i+1]-t[i])*(py_po1[i+1]**2+py_po1[i]**2) + 0.5*(t[i+1]-t[i])*(px_po1[i+1]**2+px_po1[i]**2)
-    q2 = q2+ 0.5*(t[i+1]-t[i])*(py_po2[i+1]**2+py_po2[i]**2) + 0.5*(t[i+1]-t[i])*(px_po2[i+1]**2+px_po2[i]**2)
-    q3 = q3+ 0.5*(t[i+1]-t[i])*(py_po3[i+1]**2+py_po3[i]**2) + 0.5*(t[i+1]-t[i])*(px_po3[i+1]**2+px_po3[i]**2)
+for i in range(len(px_po1)-1):
+    q1 = q1+ 0.5*(t1[i+1]-t1[i])*(py_po1[i+1]**2+py_po1[i]**2) + 0.5*(t1[i+1]-t1[i])*(px_po1[i+1]**2+px_po1[i]**2)
+for i in range(len(px_po2)-1):
+    q2 = q2+ 0.5*(t2[i+1]-t2[i])*(py_po2[i+1]**2+py_po2[i]**2) + 0.5*(t2[i+1]-t2[i])*(px_po2[i+1]**2+px_po2[i]**2)
+for i in range(len(px_po3)-1):
+    q3 = q3+ 0.5*(t3[i+1]-t3[i])*(py_po3[i+1]**2+py_po3[i]**2) + 0.5*(t3[i+1]-t3[i])*(px_po3[i+1]**2+px_po3[i]**2)
     
 print("flux is (%s,%s,%s)" % (q1,q2,q3))
     

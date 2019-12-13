edd374d43b6e6b71b2bb24fab66dc27ab4a839f8
Wenyang Lyu <wl16298@bristol.ac.uk>
2019-12-13 19:36:56 +0000

    update calculation for reaction probability

--- depth_flatness/samplingtrajectories2.py
+++ depth_flatness/samplingtrajectories2.py
@@ -11,7 +11,6 @@ from IPython.core.display import HTML
 from mpl_toolkits.mplot3d import Axes3D
 from numpy import linalg as LA
 import scipy.linalg as linalg
-import time
 from functools import partial
 import numpy as np
 import matplotlib.pyplot as plt
@@ -87,19 +86,35 @@ def get_pot_surf_proj(xVec, yVec,par):
     return surfProj
 
 def deter_py(x,y,px,e,par):
-    # this function determines the postive value of py for a given x,y,px and total energy e.
+    """ this function determines the postive value of py for a given x,y,px and total energy e.
+    
+    Parameters:
+        x: initial condition of x coordinate
+        y: initial condition of y coordinate
+        px: initial condition of px coordinate
+        e: total energy of the system
+        par: parameter values
+    """
     py = np.sqrt(e-V_SN2dof(x,y,par)-0.5*px**2)
     return py
 
-#%%
-#parameters = np.array([MASS_A, MASS_B, EPSILON_S, 5, mu, 0.01, omega]);
-def reaction_probability(x,y,e,par,num_sim,react_time_file):
-    # This function records the time when the trajectory reaches the DS(x=0) from some initial conditions in the reactants region(x>0).
-    # num_sim is the number of simulations we want to perform at a particular point on the configuration space 
+#%% defines function for calculating reaction probability, sampling points in the configuration space
+def reaction_probability(x,y,e,par,num_sim):
+    """
+    This function records the time when the trajectory reaches the DS(x=0) 
+    from some initial conditions in the reactants region(x>0).
+    num_sim is the number of simulations we want to perform at a particular point on the configuration space 
+    Parameters:
+        x: initial condition of x coordinate
+        y: initial condition of y coordinate
+        e: total energy of the system
+        par: parameter values
+        num_sim: number of simulation we want to perform for given values of x, y.
+    """
     TSPAN = [0,40]
     RelTol = 3.e-10
     AbsTol = 1.e-10
-    px_max = deter_py(x,y,0,e,par) # maximum value of px we can get for a fixed x, y, and total energy e.
+    px_max = deter_py(x,y,0,e,par) # maximum value of px we can get for a fixed x, y, total energy e and parameter values.
 #    px = np.linspace(0,px_max,num_sim) # sampling positive px
     px = np.linspace(-px_max,0,num_sim) # sampling negative px
     event_t = np.zeros((num_sim,1))
@@ -107,10 +122,12 @@ def reaction_probability(x,y,e,par,num_sim,react_time_file):
         print(i)
         f1 = partial(saddlenode2dof, par=parameters) 
         reaction = partial(reaction_event, par=parameters)
-        soln1 = solve_ivp(f1, TSPAN, [x,y,px[i],deter_py(x,y,px[i],e,par)],method='RK45',dense_output=True, events = reaction,rtol=RelTol, atol=AbsTol)
+        soln1 = solve_ivp(f1, TSPAN, [x,y,px[i],deter_py(x,y,px[i],e,par)], \
+                                      method='RK45',dense_output=True, events = reaction,rtol=RelTol, atol=AbsTol)
         te1 = soln1.t_events[0]
         react_number=0
-        if len(te1) > 0: # there is a possibility that the trajectory won't react, i.e. it will not reach the DS(x=0), which means te1 is [](empty).
+        if len(te1) > 0: # there is a possibility that the trajectory won't react,
+                         # i.e. it will not reach the DS(x=0), which means te1 is [](empty).
             event_t[i] = te1[0]
             react_number = react_number+1
             
@@ -120,6 +137,33 @@ def reaction_probability(x,y,e,par,num_sim,react_time_file):
     print("exits products region at time%s" %(te1))
     return event_t
 
+def sampling_configuration(x_min,x_max,y_min,y_max,num_pts,par,e):
+    """This function returns the sampling points for calculating reaction probability in configuration space x-y.
+       Returns all the points in the domain x \in [x_min, x_max], y \in [y_min, y_max] s.t. V(x,y) <=e
+       
+       Variables:
+           x_min, x_max, y_min, y_max define a domain in the configuration space x-y
+           num_pts is the number of points in each x, y direction.(In total there are num_pts**2 points in the domain)
+           par is the parameter values
+           e is the total energy of the system
+           (all the points in the configuration space have energy <= e)
+       
+       Returns:
+           accept_pts[:num_accept_pts,:] are the positions of all possible sampling points 
+           accept_pts[:num_accept_pts,0] is the x coordinate of the sampling points
+           accept_pts[:num_accept_pts,1] is the y coordinate of the sampling points
+           num_accept_pts is the total number of accepted points(out of num_pts**2 points).
+    """
+    x = np.linspace(x_min,x_max,num_pts)
+    y = np.linspace(y_min,y_max,num_pts)
+    num_accept_pts = 0
+    accept_pts = np.zeros((num_pts**2,2))
+    for i in range(num_pts):
+        for j in range(num_pts):
+            if V_SN2dof(x[i],y[j],par) <= e:
+                num_accept_pts = num_accept_pts+1
+                accept_pts[num_accept_pts,:] = [x[i],y[j]]
+    return accept_pts[:num_accept_pts,:]
 #%%
 num_sim=100 # The expected time to run the simulation for 3 different alphas, when num_sim=100 is approx 100 min.
 e=0.5
@@ -220,60 +264,24 @@ event_t3 = np.loadtxt(react_time_file.name)
 event_t3 = event_t3[num_sim:]
 react_time_file.close()
 
-#%%
+#%% Plot the reaction probability as a function of time
 axis_fs=30
 event_t1 = np.sort(event_t1,axis=None)
 event_t2 = np.sort(event_t2,axis=None)
 event_t3 = np.sort(event_t3,axis=None)
+event_time = [event_t1, event_t2, event_t3]
+alpha = [1,2,5]
 ax = plt.gca()
-#plot1
-non_react1=0
-for i in range(len(event_t1)):
-    if event_t1[i-1] ==0 and event_t1[i] !=0:
-        non_react1 = i
-        
-        
-react_time1=event_t1[non_react1:]
-react_prob1 = np.zeros((len(react_time1),1))
-for i in range(len(react_time1)):
-    react_prob1[i] = (i+1)/len(event_t1)
-ax.plot(react_time1,react_prob1, label=r'$\alpha=%s,\mu=%s,\epsilon=%s,\omega=%s,e=%s$' %(alpha1,parameters[4],epsilon,parameters[6],e))
 
-#plot2
-non_react2=0
-for i in range(len(event_t2)):
-    if event_t2[i-1] ==0 and event_t2[i] !=0:
-        non_react2 = i
-        
-react_time2=event_t2[non_react2:]
-react_prob2 = np.zeros((len(react_time2),1))
-for i in range(len(react_time2)):
-    react_prob2[i] = (i+1)/len(event_t2)
-ax.plot(react_time2,react_prob2, label=r'$\alpha=%s,\mu=%s,\epsilon=%s,\omega=%s,e=%s$' %(alpha2,parameters[4],epsilon,parameters[6],e))
-
-#plot3
-non_react3=0
-for i in range(len(event_t3)):
-    if event_t3[i-1] ==0 and event_t3[i] !=0:
-        non_react3 = i
-        
-react_time3=event_t3[non_react3:]
-react_prob3 = np.zeros((len(react_time3),1))
-for i in range(len(react_time3)):
-    react_prob3[i] = (i+1)/len(event_t3)
-ax.plot(react_time3,react_prob3, label=r'$\alpha=%s,\mu=%s,\epsilon=%s,\omega=%s,e=%s$' %(alpha3,parameters[4],epsilon,parameters[6],e))
-#num_sim=50
-#event_t = reaction_probability(-0.1,0.5,0.5,parameters,num_sim)
-#event_t = np.sort(event_t,axis=None)
-#ax = plt.gca()
-#for i in range(len(event_t)):
-#    if event_t[i-1] ==0 and event_t[i] !=0:
-#        non_react = i
-#        
-#react_time=event_t[non_react:]
-#react_prob = np.zeros((len(react_time),1))
-#for i in range(len(react_time)):
-#    react_prob[i] = (i+1)/num_sim
+for i in range(3):
+    event_t = event_time[i]
+    non_react = len(np.where(event_t==0)[0])
+    react_time=event_t[non_react:]
+    react_prob = np.zeros((len(react_time),1))
+    for j in range(len(react_time)):
+        react_prob[j] = (j+1)/len(event_t)
+    ax.plot(react_time,react_prob, label=r'$\alpha=%s,\mu=%s,\epsilon=%s,\omega=%s,e=%s$'\
+            %(alpha[i],parameters[4],epsilon,parameters[6],e))
 
 ax.set_xlabel('$t$', fontsize=axis_fs)
 ax.set_ylabel('reaction probability', fontsize=axis_fs)
@@ -288,9 +296,9 @@ MASS_B=1.0
 EPSILON_S=0.0
 alpha=1.0
 #mu=alpha**(3/4)
-mu = 0.1
-omega=1.0
-epsilon=1.5
+mu = 4
+omega=3.0
+epsilon=0
 
 parameters = np.array([MASS_A, MASS_B, EPSILON_S, alpha, mu, epsilon, omega]);
 
@@ -298,53 +306,78 @@ def V_SN2dof(x,y,par):
     pot = (1/3)*par[3]*x**3 - math.sqrt(par[4])*x**2 + 0.5*par[6]*y**2+0.5*par[5]*(x-y)**2
     return pot
 
-xVec = np.linspace(-3,3,resX)
+xVec = np.linspace(-2,6,resX)
 yVec = np.linspace(-3,3,resX)
 xMat, yMat = np.meshgrid(xVec, yVec)
 
 ax = plt.gca()
-
 # projection of the PES on the x-y configuration space
-cset1 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), -0.5,linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
-cset2 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), 0.0,linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
-cset3 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), 0.5,linewidths = 1.0, colors = 'b', alpha = 0.8)
-cset5 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), 1.0,linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
+cset1 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), [-0.5],\
+                   linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
+cset2 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), [0.0],\
+                   linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
+cset3 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), [0.5],\
+                   linewidths = 1.0, colors = 'b', alpha = 0.8)
+cset5 = ax.contour(xMat, yMat, get_pot_surf_proj(xVec, yVec,parameters), [1.0],\
+                   linewidths = 1.0, cmap=cm.viridis, alpha = 0.8)
+        
+def sampling_configuration(x_min,x_max,y_min,y_max,num_pts,par,e):
+    """This function returns the sampling points for calculating reaction probability in configuration space x-y.
+       Returns all the points in the domain x \in [x_min, x_max], y \in [y_min, y_max] s.t. V(x,y) <=e
+       
+       Variables:
+           x_min, x_max, y_min, y_max define a domain in the configuration space x-y
+           num_pts is the number of points in each x, y direction.(In total there are num_pts**2 points in the domain)
+           par is the parameter values
+           e is the total energy of the system
+           (all the points in the configuration space have energy <= e)
+       
+       Returns:
+           accept_pts[:num_accept_pts,:] are the positions of all possible sampling points 
+           accept_pts[:num_accept_pts,0] is the x coordinate of the sampling points
+           accept_pts[:num_accept_pts,1] is the y coordinate of the sampling points
+           num_accept_pts is the total number of accepted points(out of num_pts**2 points).
+    """
+    x = np.linspace(x_min,x_max,num_pts)
+    y = np.linspace(y_min,y_max,num_pts)
+    num_accept_pts = 0
+    accept_pts = np.zeros((num_pts**2,2))
+    for i in range(num_pts):
+        for j in range(num_pts):
+            if V_SN2dof(x[i],y[j],par) <= e:
+                num_accept_pts = num_accept_pts+1
+                accept_pts[num_accept_pts,:] = [x[i],y[j]]
+    return accept_pts[:num_accept_pts,:]
 
-# Draw the DS&sampling points
-e=0.5 # total energy
-DS = np.linspace(-np.sqrt(2*e/(omega**2+epsilon)),np.sqrt(2*e/(omega**2+epsilon)),12)
-ax.plot(np.zeros(12),DS)
-ax.scatter(0,0.95346259, s = 100, c = 'r', marker = 'x')
-chosen_pts = np.zeros((10,10))
-for i in range(10):
-    V_max= fsolve(lambda x: V_SN2dof(x,DS[1:11][i],parameters)-e,1.5)
-    V_x_range= np.linspace(0,V_max,12)
-    chosen_pts[i] = V_x_range[1:11]
-    ax.scatter(chosen_pts[i],np.ones(10)*DS[1:11][i], s = 5, c = 'b', marker = '.')
+sampling_points = sampling_configuration(0,6,-3,3,100,parameters,0.5)
+for i in range(len(sampling_points)):
+    ax.scatter(sampling_points[i,0],sampling_points[i,1], s = 5, c = 'b', marker = '.')
 TSPAN = [0,40]
 RelTol = 3.e-10
 AbsTol = 1.e-10 
 f1 = partial(saddlenode2dof, par=parameters) 
 reaction = partial(reaction_event, par=parameters)
-# pick a x and y values on the configuration space.
-pick_x = chosen_pts[0,1]
-pick_y = DS[1]
-# integrate the Hamilton's equation and plot the trajectory
-soln1 = solve_ivp(f1, TSPAN, [pick_x,pick_y,-0.11,deter_py(pick_x,pick_y,-0.11,0.5,parameters)],method='RK45',dense_output=True, events = reaction,rtol=RelTol, atol=AbsTol)
+# randomly pick a x and y values on the configuration space.
+pick_x = sampling_points[int(len(sampling_points)/102),0]
+pick_y = sampling_points[int(len(sampling_points)/102),1]
+# integrate the Hamilton's equation and plot the trajectory, with negative px
+soln1 = solve_ivp(f1, TSPAN, [pick_x,pick_y,-0.1,-deter_py(pick_x,pick_y,-0.1,0.5,parameters)],\
+                              method='RK45',dense_output=True, events = reaction,rtol=RelTol, atol=AbsTol)
 te1 = soln1.t_events[0]
 coordinates = soln1.y
 ax.plot(coordinates[0,:],coordinates[1,:])
 
-# Position of the equilibrium point
 def equ_pos(par):
+    """Returns the position of the centre equilibrium point
+    """
     xe = 2*math.sqrt(par[4])/par[3] - (par[6]**2*par[5])/(par[3]*(par[6]**2+par[5]))
     ye = xe*par[5]/(par[6]**2+par[5])
     return xe, ye
 
-ax.set_xlim(-3, 3)
+ax.set_xlim(-2, 6)
 ax.set_ylim(-3, 3)
 ax.scatter(0,0, s = 100, c = 'r', marker = 'x')
-ax.scatter(equ_pos(parameters)[0],equ_pos(parameters)[1], s = 100, c = 'r', marker = '.')
-ax.set_title(r'$\alpha=1, \mu=0.1$')
+ax.scatter(equ_pos(parameters)[0],equ_pos(parameters)[1], s = 100, c = 'r', marker = 'o')
+ax.set_title(r'$\alpha=%s, \mu=%s$' %(alpha, mu))
 plt.show()
 

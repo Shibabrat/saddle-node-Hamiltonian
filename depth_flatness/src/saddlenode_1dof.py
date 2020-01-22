222c6575fdeacb9b02afaa33b9e308290b639d3d
Wenyang Lyu <wl16298@bristol.ac.uk>
2020-01-22 14:22:01 +0000

    restruc repo

---
+++ depth_flatness/src/saddlenode_1dof.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 14:19:35 2020
+
+@author: wl16298
+"""
+
---
+++ depth_flatness/src/saddlenode_2dof_uncoupled.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 14:20:25 2020
+
+@author: wl16298
+"""
+
